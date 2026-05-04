f218526de396cb3c94c64cd13bcd0679bb6efadf
Marcel Franco de Oliveira <marcelfranco@id.uff.br>
2026-05-04 18:54:58 -0300

    remoção do "este.txt"

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ def init_tableClientes():
     conn.close()
     print("Banco de dados inicializado com sucesso!")   
 
-@app.route('/home', methods=['GET'])
+@app.route('/', methods=['GET'])
 def home():
     return render_template('index.html')
 
